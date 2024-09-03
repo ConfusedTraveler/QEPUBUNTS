06bc060b1e2072d471d7503cd87fc0e38cbc3841
Jamal Mohammed <mohammed@ifi.uzh.ch>
2024-09-03 16:56:08 +0200

    pimax update

--- Experiments/Exp1/pimax.py
+++ Experiments/Exp1/pimax.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('../..') 
 
 from Utils.LZ2 import * 
+from Utils.compute_PIMax import * 
 
 import numpy as np
 from scipy.optimize import fsolve
@@ -18,22 +19,23 @@ def get_data(path, length):
 
 if __name__ == "__main__":
 
-    N_values = [5,10,15,20,25,30,35,40,45,50]
-    length = 10000
+    N_values = [50,45,40,35,30,25,20,15,10,5]
+    length = 20000
     n=50000
-    a_values = [1,15]
+    a_values = [15,1]
     for a in a_values:
         PM = []
         for N in N_values:
         	
             input_path = "../../Datasets/Markov/Exp1/markov_ts_"+str(N)+"_"+str(n)+"_"+str(a)+".csv"
             series = get_data(input_path, length)
+            print("Running... N=",N,"a=",a)
             
             H = Compute_LZ2(series[:int(0.8*len(series))],0.0)
             pimax = get_pimax(H,N)
             print(pimax)
             PM.append(pimax)
-            print("Done! N=","a=",a)
+            
         
         output_path = "Results/PIMAX/pimax_"+str(N)+"_"+str(n)+"_"+str(a)+".csv"
 
