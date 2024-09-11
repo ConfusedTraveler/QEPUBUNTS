7e90d9ba81942a5386cfd50342b05ca74e5e090c
Jamal Mohammed <mohammed@ifi.uzh.ch>
2024-09-11 16:00:22 +0200

    add doc-string

--- Challenges_section/fig2.py
+++ Challenges_section/fig2.py
@@ -24,6 +24,23 @@ seed = 37
 #Function to generate transition matrix
 def generate_transition_matrix(N,p):
 
+    """
+    Generate a transition matrix of size NxN with a probability p of
+    transitioning to a randomly chosen state, and a probability (1-p) of
+    equiprobably transitioning to any of the other states
+
+    Parameters
+    ----------
+    N : int
+        Size of the transition matrix
+    p : float
+        Probability of transitioning to a randomly chosen state
+
+    Returns
+    -------
+    P : array, shape (N, N)
+        Transition matrix
+    """
     P = np.zeros((N, N))
     np.random.seed(seed)
 
@@ -45,6 +62,22 @@ def generate_transition_matrix(N,p):
 # Function to train a Markov model and estimate the transition matrix
 def train_markov_model(train_data, N):
     
+    """
+    Train a Markov model and estimate the transition matrix.
+
+    Parameters
+    ----------
+    train_data : array_like
+        The data to train the model on. This should be a sequence of states,
+        where each state is an integer between 0 and N-1.
+    N : int
+        The number of states in the Markov model.
+
+    Returns
+    -------
+    transition_matrix : array_like, shape (N, N)
+        The estimated transition matrix of the Markov model.
+    """
     np.random.seed(seed)
     count_matrix = np.zeros((N, N))
     for i in range(len(train_data) - 1):
@@ -58,6 +91,22 @@ def train_markov_model(train_data, N):
 
 # Function to evaluate a Markov model using the transition matrix
 def markov_model(eval_data, transition_matrix):
+    """
+    Evaluate a Markov model and generate predictions.
+
+    Parameters
+    ----------
+    eval_data : array_like
+        The data to evaluate the model on. This should be a sequence of states,
+        where each state is an integer between 0 and N-1.
+    transition_matrix : array_like, shape (N, N)
+        The transition matrix of the Markov model.
+
+    Returns
+    -------
+    predictions : array_like
+        The predicted next states of the evaluation data.
+    """
     N = len(transition_matrix)
     np.random.seed(seed)
     predictions = []
@@ -70,15 +119,49 @@ def markov_model(eval_data, transition_matrix):
     return np.array(predictions)
 
 def predict(time_series, N):
-	np.random.seed(seed)
-	n = len(time_series)
-	train_size = int(0.8 * n)
-	train_data, eval_data = time_series[:train_size], time_series[train_size:]
-	transition_matrix = train_markov_model(train_data, N)
-	predictions = markov_model(eval_data, transition_matrix)			
-	return predictions,eval_data
+    """
+    Predict the next states of a time series using a Markov model.
+
+    Parameters
+    ----------
+    time_series : array_like
+        The time series to predict.
+    N : int
+        The number of states in the Markov model.
+
+    Returns
+    -------
+    predictions : array_like
+        The predicted next states of the time series.
+    eval_data : array_like
+        The evaluation data used to train the model.
+    """
+    np.random.seed(seed)
+    n = len(time_series)
+    train_size = int(0.8 * n)
+    train_data, eval_data = time_series[:train_size], time_series[train_size:]
+    transition_matrix = train_markov_model(train_data, N)
+    predictions = markov_model(eval_data, transition_matrix)			
+    return predictions,eval_data
 
 def generate_sequence(transition_matrix, initial_state, sequence_length):
+    """
+    Generate a sequence of states using a Markov model.
+
+    Parameters
+    ----------
+    transition_matrix : array_like, shape (N, N)
+        The transition matrix of the Markov model.
+    initial_state : int
+        The initial state of the sequence.
+    sequence_length : int
+        The length of the sequence to generate.
+
+    Returns
+    -------
+    sequence : array_like, shape (sequence_length,)
+        The generated sequence of states.
+    """
     current_state = initial_state
     sequence = [current_state]
     for _ in range(sequence_length - 1):
@@ -88,22 +171,63 @@ def generate_sequence(transition_matrix, initial_state, sequence_length):
     return sequence	
 
 def get_acc(predictions,eval_data):
-	p = 0
-	for i in range(len(predictions)):
-		if predictions[i]==eval_data[i+1]:
-			p+=1		
-	acc = p/len(predictions)			
-	return acc			
+    """
+    Calculate the accuracy of a sequence of predictions against the evaluation data.
+
+    Parameters
+    ----------
+    predictions : array_like
+        The sequence of predictions.
+    eval_data : array_like
+        The evaluation data used to calculate the accuracy.
+
+    Returns
+    -------
+    accuracy : float
+        The accuracy of the predictions, calculated as the proportion of
+        correctly predicted states.
+    """
+    p = 0
+    for i in range(len(predictions)):
+        if predictions[i]==eval_data[i+1]:
+            p+=1		
+    acc = p/len(predictions)			
+    return acc			
 
 def discretize(S,pred,eval_data,e):
 	
-	N = int((max(S) - min(S) + 2 * e) / (2 * e))
-	bins = np.linspace(min(S) - e, max(S) + e, N + 1)
-	S_discretized = pd.cut(S, bins=bins, labels=False)
-	pred_discretized = pd.cut(pred, bins=bins, labels=False)
-	eval_discretized = pd.cut(eval_data, bins=bins, labels=False)
-	N = len(np.unique(S_discretized))
-	return S_discretized,pred_discretized,eval_discretized,N
+    """
+    Discretize the given sequences S, pred, and eval_data into a uniform grid of size N.
+    
+    Parameters
+    ----------
+    S : array_like
+        The sequence to discretize.
+    pred : array_like
+        The sequence of predictions to discretize.
+    eval_data : array_like
+        The evaluation data to discretize.
+    e : float
+        The precision parameter for the discretization.
+    
+    Returns
+    -------
+    S_discretized : array_like
+        The discretized sequence S.
+    pred_discretized : array_like
+        The discretized sequence of predictions.
+    eval_discretized : array_like
+        The discretized evaluation data.
+    N : int
+        The number of unique states in the discretized sequence.
+    """
+    N = int((max(S) - min(S) + 2 * e) / (2 * e))
+    bins = np.linspace(min(S) - e, max(S) + e, N + 1)
+    S_discretized = pd.cut(S, bins=bins, labels=False)
+    pred_discretized = pd.cut(pred, bins=bins, labels=False)
+    eval_discretized = pd.cut(eval_data, bins=bins, labels=False)
+    N = len(np.unique(S_discretized))
+    return S_discretized,pred_discretized,eval_discretized,N
 	
 if __name__ == "__main__":
 
